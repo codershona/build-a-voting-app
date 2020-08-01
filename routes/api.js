5a0f3dac14bc6473147f87ac87db7185e1dbe1a5
codershona <falgunimst95@gmail.com>
2020-08-01 18:39:24 +0200

    succesfully post register in api and user

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,38 @@
 var express = require('express');
 var router = express.Router({ caseSensitive: true });
+var bcrypt = require('bcrypt-nodejs');
+var jwt = require('jsonwebtoken');
+var User = require('../models/user');
 
 // Register:
 
 router.post('/register', function(request, response) {
-   console.log(request.body);
-   
+   // console.log(request.body);
+   if(request.body.name && request.body.password) {
+     var user = new User();
+     user.name = request.body.name;
+     // console.timeStart('bcryptHashing');
+     console.time('bcryptHashing');
+
+     user.password = bcrypt.hashSync(request.body.password, bcrypt.genSaltSync(10))
+     console.timeEnd('bcryptHashing');
+      user.save(function(err, document) {
+      	if(err) {
+      		return response.status(400).send(err)
+      	}
+      	else {
+      		return response.status(201).send(document)
+      	}
+      })
+      
+
+   }
+   else {
+   	return response.status(400).send({
+   		message: 'Invalid Credentials Supplied!'
+   	})
+   }
+
 });
 
 
